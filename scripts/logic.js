cecdfde858b149d436f65c8dad15427b5fd248ad
Emilio BLacksmith <emilioblacksmithlush@gmail.com>
2022-11-18 13:12:11 -0600

    style(index): add calculator baseline and style

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,1 +1,30 @@
 console.log("Hello World!");
+
+
+// Rotate Calculator with mouse position
+let constrain = 800;
+let mouseOverContainer = document.getElementById("ex1");
+let ex1Layer = document.getElementById("ex1-layer");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - (box.height / 2)) / constrain;
+  let calcY = (x - box.x - (box.width / 2)) / constrain;
+  
+  return "perspective(100px) "
+    + "   rotateX("+ calcX +"deg) "
+    + "   rotateY("+ calcY +"deg) ";
+};
+
+ function transformElement(el, xyEl) {
+  el.style.transform  = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function(e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  window.requestAnimationFrame(function(){
+    transformElement(ex1Layer, position);
+  });
+};
