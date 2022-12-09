8a5e878d6c84a2b3b834a58e665cd23dac53ced8
Emilio BLacksmith <emilioblacksmithlush@gmail.com>
2022-12-09 12:23:33 -0600

    feat(functionality): add functionality to buttons and overall calculator logic

--- scripts/logic.js
+++ scripts/logic.js
@@ -7,6 +7,7 @@ let dotAdded = false;
 // Screen System
 
 const ScreenNewNumber = document.querySelector('.current');
+const ScreenLastNumber = document.querySelector('.operation');
 
 // Buttons Number & Operations
 const Operations = document.querySelectorAll('#operation');
@@ -42,6 +43,7 @@ function appendNewNumber(number){
 
 function showCurrentNumberOnScreen(){
   ScreenNewNumber.innerText = newNumber;
+  ScreenLastNumber.innerText = lastNumber + currentOperation;
 }
 
 function makeOperation(operation){
@@ -50,29 +52,241 @@ function makeOperation(operation){
       newNumber = '0';
       lastNumber = '';
       currentOperation = '';
+      dotAdded = false;
       showCurrentNumberOnScreen();
       break;
     case 'C':
+      let lastChar = newNumber.slice(-1);
       newNumber = newNumber.slice(0, -1);
       if(newNumber === ''){
         newNumber = '0';
       }
+
+      if(lastChar === '.')  dotAdded = false;
       showCurrentNumberOnScreen();
       break;
+    case '.':
+      if(!dotAdded){
+        newNumber += '.';
+        dotAdded = true;
+        showCurrentNumberOnScreen();
+      }
+      break;
+    case '^':
+      if(lastNumber !== ''){
+        QuickOperationPower();
+      }else{
+        currentOperation = '^';
+        lastNumber = newNumber;
+        newNumber = '0';
+        showCurrentNumberOnScreen();
+      }
+      break;
+    case '%':
+      if(lastNumber !== ''){
+        QuickOperationPercentage();
+      }else{
+        currentOperation = '%';
+        lastNumber = newNumber;
+        newNumber = '0';
+        showCurrentNumberOnScreen();
+      }
+      break;
+    case '=':
+      OperationEqual();
+      break;
+    case '/':
+      if(lastNumber !== ''){
+        QuickOperationDivision();
+      }else{
+        currentOperation = '/';
+        lastNumber = newNumber;
+        newNumber = '0';
+        showCurrentNumberOnScreen();
+      }
+      break;
+    case '*':
+      if(lastNumber !== ''){
+        QuickOperationMultiply();
+      }else{
+        currentOperation = '*';
+        lastNumber = newNumber;
+        newNumber = '0';
+        showCurrentNumberOnScreen();
+      }
+      break;
+    case '-':
+      if(lastNumber !== ''){
+        QuickOperationMinus();
+      }else{
+        currentOperation = '-';
+        lastNumber = newNumber;
+        newNumber = '0';
+        showCurrentNumberOnScreen();
+      }
+      break;
+    case '+':
+      if(lastNumber !== ''){
+        QuickOperationPlus();
+      }else{
+        currentOperation = '+';
+        lastNumber = newNumber;
+        newNumber = '0';
+        showCurrentNumberOnScreen();
+      }
+      break;
     default:
-      console.log("lol");
       break;
   }
 }
 
+//#region Operations
+
+function OperationPower(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  newNumber = parseFloat(lastNum) ** parseFloat(newNum);
+  lastNumber = '';
+  currentOperation = '';
+  showCurrentNumberOnScreen();
+}
+
+function OperationPercentage(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  newNumber = parseFloat(lastNum) % parseFloat(newNum);
+  lastNumber = '';
+  currentOperation = '';
+  showCurrentNumberOnScreen();
+}
+
+function OperationDivision(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  newNumber = parseFloat(lastNum) / parseFloat(newNum);
+  lastNumber = '';
+  currentOperation = '';
+  showCurrentNumberOnScreen();
+}
+
+function OperationMultiply(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  newNumber = parseFloat(lastNum) * parseFloat(newNum);
+  lastNumber = '';
+  currentOperation = '';
+  showCurrentNumberOnScreen();
+}
+
+function OperationMinus(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  newNumber = parseFloat(lastNum) - parseFloat(newNum);
+  lastNumber = '';
+  currentOperation = '';
+  showCurrentNumberOnScreen();
+}
+
+function OperationPlus(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  newNumber = parseFloat(lastNum) + parseFloat(newNum);
+  lastNumber = '';
+  currentOperation = '';
+  showCurrentNumberOnScreen();
+}
+
+function OperationEqual(){
+  switch(currentOperation){
+    case '^':
+      OperationPower();
+      break;
+    case '%':
+      OperationPercentage();
+      break;
+    case '/':
+      OperationDivision();
+      break;
+    case '*':
+      OperationMultiply();
+      break;
+    case '-':
+      OperationMinus();
+      break;
+    case '+':
+      OperationPlus();
+      break;
+    default:
+      break;
+  }
+}
+//#endregion
+
+//#region Quick Operations
+
+function QuickOperationPower(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  lastNumber = parseFloat(lastNum) ** parseFloat(newNum);
+  currentOperation = '^';
+  newNumber = '0';
+  showCurrentNumberOnScreen();
+}
+
+function QuickOperationPercentage(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  lastNumber = parseFloat(lastNum) % parseFloat(newNum);
+  currentOperation = '%';
+  newNumber = '0';
+  showCurrentNumberOnScreen();
+}
+
+function QuickOperationDivision(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  lastNumber = parseFloat(lastNum) / parseFloat(newNum);
+  currentOperation = '/';
+  newNumber = '0';
+  showCurrentNumberOnScreen();
+}
+
+function QuickOperationMultiply(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  lastNumber = parseFloat(lastNum) * parseFloat(newNum);
+  currentOperation = '*';
+  newNumber = '0';
+  showCurrentNumberOnScreen();
+}
+
+function QuickOperationMinus(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  lastNumber = parseFloat(lastNum) - parseFloat(newNum);
+  currentOperation = '-';
+  newNumber = '0';
+  showCurrentNumberOnScreen();
+}
+
+function QuickOperationPlus(){
+  let newNum = newNumber;
+  let lastNum = lastNumber;
+  lastNumber = parseFloat(lastNum) + parseFloat(newNum);
+  currentOperation = '+';
+  newNumber = '0';
+  showCurrentNumberOnScreen();
+}
+//#endregion
+
 function handleKeyboardInput(e) {
   if (e.key >= 0 && e.key <= 9) appendNewNumber(e.key)
-  if (e.key === '.') /*appendPoint()*/
-  if (e.key === '=' || e.key === 'Enter') //evaluate()
-  if (e.key === 'Backspace') //deleteNumber()
-  if (e.key === 'Escape') //clear()
-  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
-    //setOperation(convertOperator(e.key))
+  if (e.key === '.') makeOperation('.');
+  if (e.key === '=' || e.key === 'Enter') makeOperation('=');
+  if (e.key === 'Backspace') makeOperation('C')
+  if (e.key === 'Escape') makeOperation('AC');
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/' || e.key === '%' || e.key === '^')
+    makeOperation(e.key);
   ;
 }
 
